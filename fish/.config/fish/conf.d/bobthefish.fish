fd55a8a091ea0851aa196f6567f3a64864ef2318
tsukachu <concon.ch.1543@gmail.com>
2025-11-30 01:49:57 +0900

    右プロンプトの設定を追加

--- fish/.config/fish/conf.d/bobthefish.fish
+++ fish/.config/fish/conf.d/bobthefish.fish
@@ -1,2 +1,8 @@
 set -g theme_powerline_fonts no
 set -g theme_nerd_fonts yes
+
+# 右プロンプト
+## 日付フォーマット
+set -g theme_date_format "+%Y年%m月%d日(%a) %H:%M"
+## コマンド実行時間
+set -g theme_display_cmd_duration yes
