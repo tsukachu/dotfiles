5a60d3470629abed79af14cd3ef9086a4273da9e
tsukachu <concon.ch.1543@gmail.com>
2026-02-05 00:30:53 +0900

    遅延ロードをしないように

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,76 +5,14 @@ if status is-interactive
     # pyenv
     set -x PYENV_ROOT $HOME/.pyenv # config で都度読み込まれるので-U/--universal は外す
     test -d $PYENV_ROOT/bin; and fish_add_path $PYENV_ROOT/bin
-
-    ## 遅延ロード
-    function __pyenv_load
-        if set -q __pyenv_loaded
-            return
-        end
-
-        set -g __pyenv_loaded true
-
-        pyenv init - fish | source
-        pyenv virtualenv-init - | source
-    end
-
-    ## 関連するコマンドを遅延ロードの対象にする
-    ## FIXME: DRY
-    function pyenv
-        functions -e pyenv
-        __pyenv_load
-        command pyenv $argv
-    end
-    function python
-        functions -e python
-        __pyenv_load
-        command python $argv
-    end
-    function pip
-        functions -e pip
-        __pyenv_load
-        command pip $argv
-    end
+    pyenv init - fish | source
+    # pyenv-virtualenv
+    pyenv virtualenv-init - | source
 
     #nodenv
     set -x NODENV_ROOT $HOME/.nodenv # config で都度読み込まれるので-U/--universal は外す
     test -d $NODENV_ROOT/bin; and fish_add_path $NODENV_ROOT/bin
-
-    ## 遅延ロード
-    function __nodenv_load
-        if set -q __nodenv_loaded
-            return
-        end
-
-        set -g __nodenv_loaded true
-
-        nodenv init - fish | source
-    end
-
-    ## 関連するコマンドを遅延ロードの対象にする
-    ## FIXME: DRY
-    function nodenv
-        functions -e nodenv
-        __nodenv_load
-        command nodenv $argv
-    end
-    function node
-        functions -e node
-        __nodenv_load
-        command node $argv
-    end
-    function npm
-        functions -e npm
-        __nodenv_load
-        command npm $argv
-    end
-
-    function make
-        functions -e make
-        __pyenv_load
-        __nodenv_load
-        command make $argv
-    end
+    nodenv init - fish | source
 end
 
 # Intel/Apple Silicon でパスが変わるので
